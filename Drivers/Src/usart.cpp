53a7345c27f08214b7893df06e36822b763fcbe8
vaishavs <53684182+vaishavs@users.noreply.github.com>
2022-07-12 10:44:21 +0530

    Update usart.cpp

--- Drivers/Src/usart.cpp
+++ Drivers/Src/usart.cpp
@@ -36,9 +36,9 @@ void USART::DeInit()
 {
   ClockControl(DIS);
 
-  if(pReg == USART1) RCC->APB2RSTR.reset(RCC_APB2RSTR_USART1RST_Pos);
-  if(pReg == USART2) RCC->APB1RSTR.reset(RCC_APB1RSTR_USART2RST_Pos);
-  if(pReg == USART6) RCC->APB2RSTR.reset(RCC_APB2RSTR_USART6RST_Pos);
+  if(pReg == USART1) RCC->APB2RSTR.set(RCC_APB2RSTR_USART1RST_Pos);
+  if(pReg == USART2) RCC->APB1RSTR.set(RCC_APB1RSTR_USART2RST_Pos);
+  if(pReg == USART6) RCC->APB2RSTR.set(RCC_APB2RSTR_USART6RST_Pos);
 
   delete pReg;
 }
