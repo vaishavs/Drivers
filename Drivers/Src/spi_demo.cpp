868391ea4c1269ada6bd285dd83d852102733af9
vaishavs <avsvaishnavi@gmail.com>
2023-06-22 15:46:16 +0530

    Update spi_demo.cpp

--- Drivers/Src/spi_demo.cpp
+++ Drivers/Src/spi_demo.cpp
@@ -19,11 +19,11 @@ void GPIO_Init();
 void GPIO_Button_Init(GPIO button);
 void SPI_Init();
 
-SPI spi2(SPI2);
+SPI spi2(SPI2, SPI_DEV_MASTER, SPI_BUS_FD, FREQ_DIV_2, SPI_DFF_16, LOW, FALL, DIS);
 
 int main(void)
 {
-	GPIO button(GPIOC,13);
+	GPIO button(GPIOC,13,GPIO_SPEED_HIGH, GPIO_PULL_NONE);
 
 	GPIO_Init();
 	GPIO_Button_Init(button);
@@ -50,31 +50,31 @@ void GPIO_Init()
 {
 	//PB12-NSS, PB13-SCK, PB14-MISO, PB15-MOSI; AF5
 
-	GPIO nss(GPIOB,12);
-	nss.setMode(GPIO_MODE_ALTFN, (uint8_t)5);
-	nss.Init(GPIO_SPEED_HIGH, GPIO_PULL_NONE);
+	GPIO nss(GPIOB,12, GPIO_SPEED_HIGH, GPIO_PULL_NONE);
+	nss.setMode(GPIO_MODE_ALTFN, 5);
+	nss.Init();
 
-	GPIO sck(GPIOB,13);
-	sck.setMode(GPIO_MODE_ALTFN, (uint8_t)5);
-	sck.Init(GPIO_SPEED_HIGH, GPIO_PULL_NONE);
+	GPIO sck(GPIOB,13,GPIO_SPEED_HIGH, GPIO_PULL_NONE);
+	sck.setMode(GPIO_MODE_ALTFN, 5);
+	sck.Init();
 
-	GPIO miso(GPIOB,14);
-	miso.setMode(GPIO_MODE_ALTFN, (uint8_t)5);
-	miso.Init(GPIO_SPEED_HIGH, GPIO_PULL_NONE);
+	GPIO miso(GPIOB,14,GPIO_SPEED_HIGH, GPIO_PULL_NONE);
+	miso.setMode(GPIO_MODE_ALTFN, 5);
+	miso.Init();
 
-	GPIO mosi(GPIOB,15);
-	mosi.setMode(GPIO_MODE_ALTFN, (uint8_t)5);
-	mosi.Init(GPIO_SPEED_HIGH, GPIO_PULL_NONE);
+	GPIO mosi(GPIOB,15,GPIO_SPEED_HIGH, GPIO_PULL_NONE);
+	mosi.setMode(GPIO_MODE_ALTFN, 5);
+	mosi.Init();
 }
 
 void GPIO_Button_Init(GPIO button)
 {
 	//Initialize User button: PC13
 	button.setMode(GPIO_MODE_INPUT); //Set as input pin
-	button.Init(GPIO_SPEED_HIGH, GPIO_PULL_NONE); //Initialize rest of the parameters
+	button.Init(); //Initialize rest of the parameters
 }
 
 void SPI_Init()
 {
-	spi2.Init(SPI_DEV_MASTER, SPI_BUS_FD, FREQ_DIV_2, SPI_DFF_16, LOW, FALL, DIS);
+	spi2.Init();
 }
