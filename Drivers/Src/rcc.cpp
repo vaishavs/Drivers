660833e494eeda5f0ffda948f8afe8f6db8ec7ff
vaishavs <avsvaishnavi@gmail.com>
2023-07-27 12:22:38 +0530

    Update rcc.cpp

--- Drivers/Src/rcc.cpp
+++ Drivers/Src/rcc.cpp
@@ -6,7 +6,7 @@
  */
 
 
-#include "stm32f411xe.h"
+#include "main.h"
 #include "rcc.h"
 
 uint32_t SystemCoreClock = 16000000;
@@ -25,14 +25,16 @@ uint32_t GetPLLClock()
   if(RCC->PLLCFGR[RCC_PLLCFGR_PLLSRC_Pos] != RCC_PLLCFGR_PLLSRC_HSI)
   {
      // HSE used as PLL clock source
-     pllvco = (uint32_t) ((uint64_t) 8000000U * ((uint64_t) (RCC->PLLCFGR[RCC_PLLCFGR_PLLN_Pos])) / (uint64_t)pllm);
+  	 uint32_t plln = read_value(RCC->PLLCFGR, RCC_PLLCFGR_PLLN, RCC_PLLCFGR_PLLN_Pos);
+     pllvco = (uint32_t) ((uint64_t) 8000000U * ((uint64_t) plln) / (uint64_t)pllm);
   }
   else
   {
      // HSI used as PLL clock source
-     pllvco = (uint32_t) ((uint64_t) 16000000U * ((uint64_t) (RCC->PLLCFGR[RCC_PLLCFGR_PLLN_Pos])) / (uint64_t)pllm);
+  	 uint32_t plln = read_value(RCC->PLLCFGR, RCC_PLLCFGR_PLLN, RCC_PLLCFGR_PLLN_Pos);
+     pllvco = (uint32_t) ((uint64_t) 16000000U * ((uint64_t) plln) / (uint64_t)pllm);
   }
-  pllp = ((RCC->PLLCFGR[RCC_PLLCFGR_PLLP_Pos] + 1U) *2U);
+  pllp = ((read_value(RCC->PLLCFGR, RCC_PLLCFGR_PLLP, RCC_PLLCFGR_PLLP_Pos) + 1U) *2U);
 
   sysclockfreq = pllvco/pllp;
 
@@ -44,7 +46,7 @@ uint32_t GetSysClockFreq()
   uint32_t SystemClock;
   uint8_t clksrc;
 
-  clksrc = (RCC->CFGR.to_ulong() & RCC_CFGR_SWS); //Read bits 2 and 3
+  clksrc = READ_BITS(RCC->CFGR, RCC_CFGR_SWS); //Read bits 2 and 3
 
   if(clksrc == RCC_CFGR_SWS_HSI) //HSI
     SystemClock = 16000000;
@@ -58,19 +60,18 @@ uint32_t GetSysClockFreq()
 
 uint32_t GetHClock()
 {
-  uint32_t temp = (RCC->CFGR.to_ulong() & RCC_CFGR_HPRE) >> RCC_CFGR_HPRE_Pos;
+  uint32_t temp = read_value(RCC->CFGR, RCC_CFGR_HPRE, RCC_CFGR_HPRE_Pos);
   return (GetSysClockFreq() >> AHBPrescTable[temp]);
 }
 
 uint32_t GetPClock1()
 {
-  uint32_t temp = (RCC->CFGR.to_ulong() & RCC_CFGR_PPRE1) >> RCC_CFGR_PPRE1_Pos;
+  uint32_t temp = read_value(RCC->CFGR, RCC_CFGR_PPRE1, RCC_CFGR_PPRE1_Pos);
   return (GetHClock() >> APBPrescTable[temp]);
 }
 
 uint32_t GetPClock2()
 {
-  uint32_t temp = (RCC->CFGR.to_ulong() & RCC_CFGR_PPRE2) >> RCC_CFGR_PPRE2_Pos;
+  uint32_t temp = read_value(RCC->CFGR, RCC_CFGR_PPRE2, RCC_CFGR_PPRE2_Pos);
   return (GetHClock() >> APBPrescTable[temp]);
 }
-
